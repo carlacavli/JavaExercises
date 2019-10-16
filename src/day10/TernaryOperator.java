994923079d881572d37134d3f3b79e4225ee85f3
TechnoStudy <info@techno.study>
2019-10-15 21:41:16 -0400

    day 9 changes, done together in the class

--- src/day10/TernaryOperator.java
+++ src/day10/TernaryOperator.java
@@ -6,7 +6,17 @@ public class TernaryOperator {
     public static void main(String[] args) {
         int a = 2;
         int b = 5;
+
+        // here the question mark means if
+        // : symbol means else
         int min = (a <= b) ? a : b;
+
+        if (a <= b) {
+            min = a;
+        } else {
+            min = b;
+        }
+
         System.out.println(min);
     }
 }
--- src/day9/HomeworkIfElseSwitch.java
+++ src/day9/HomeworkIfElseSwitch.java
@@ -227,43 +227,43 @@ public class HomeworkIfElseSwitch {
 ////4. No Output
 //
 //    class MainClass {
-        public static void main(String arg[])
-        {
-            char stream = 'C';
-            int x = 2;
-
-            switch (x) {
-                case 1:
-                    System.out.println("SCIENCE, MATHS, PHYSICS");
-                    break;
-                case 2:
-                    switch (stream) {
-                        case 'A':
-                            System.out.println("Welcome");
-                            break;
-                        case 'C':
-                            System.out.println("TechnoStudy");
-                            break;
-                        case 'B':
-                            System.out.println("Have a nice day");
-                            break;
-                    }
-                    break;
-                case 3:
-                    switch (stream) {
-                        case 'C':
-                            System.out.println("Welcome");
-                            break;
-                        case 'D':
-                            System.out.println("In");
-                            break;
-                        case 'E':
-                            System.out.println("GFG");
-                            break;
-                    }
-                    break;
-            }
-        }
+//        public static void main(String arg[])
+//        {
+//            char stream = 'C';
+//            int x = 2;
+//
+//            switch (x) {
+//                case 1:
+//                    System.out.println("SCIENCE, MATHS, PHYSICS");
+//                    break;
+//                case 2:
+//                    switch (stream) {
+//                        case 'A':
+//                            System.out.println("Welcome");
+//                            break;
+//                        case 'C':
+//                            System.out.println("TechnoStudy");
+//                            break;
+//                        case 'B':
+//                            System.out.println("Have a nice day");
+//                            break;
+//                    }
+//                    break;
+//                case 3:
+//                    switch (stream) {
+//                        case 'C':
+//                            System.out.println("Welcome");
+//                            break;
+//                        case 'D':
+//                            System.out.println("In");
+//                            break;
+//                        case 'E':
+//                            System.out.println("GFG");
+//                            break;
+//                    }
+//                    break;
+//            }
+//        }
 //    }
 ////    Options:
 ////1. Compile time error
