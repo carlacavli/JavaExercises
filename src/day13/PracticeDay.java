55508e7ccc72cc68e43bb9566773a06516a3bbe0
TechnoStudy <info@techno.study>
2019-10-18 13:37:47 -0400

    day 11

---
+++ src/day13/PracticeDay.java
@@ -0,0 +1,32 @@
+package day13;
+
+/**
+ * TODO practice topics:
+ * if, else-if, else, nested if
+ * for, while, do-while
+ * string methods
+ * primitive data types
+ * variable assigning, reuse
+ * arrays 1D, 2D
+ */
+public class PracticeDay {
+    //TODO Task
+    // 1. write program that prints this using loops
+    //    *
+    //    **
+    //    ***
+    //    ****
+    //    *****
+    // 2. write program that prints this figures using loops
+    //        *
+    //       **
+    //      ***
+    //     ****
+    //    *****
+    //      *
+    //     * *
+    //    *   *
+    //     * *
+    //      *
+    // 3. Make interactive calculator ¯\_(ツ)_/¯
+}
