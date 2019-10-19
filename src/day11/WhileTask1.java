78f2565fb299419a92d7a9e9a0b424fc9cb2a54b
TechnoStudy <info@techno.study>
2019-10-19 16:49:18 -0400

    day 11

--- src/day11/WhileTask1.java
+++ src/day11/WhileTask1.java
@@ -5,15 +5,30 @@ import java.util.Scanner;
 public class WhileTask1 {
     // Part1, write a program which asks you to provide a text
     // and it should stop when you type "quit"
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String line = "";
 
-        System.out.println("Write a string");
-        String s = scan.nextLine();
-
-        while (!s.equals("quit")) { //s != "exit"
+        //part1
+        while (!line.equals("quit")) {
             System.out.println("Write a string");
-            s = scan.nextLine();
+            line = scan.nextLine();
+
+            // Part2, continue part1 but also check for length() of string
+            // if length is bigger than 10, program should say that string is too large
+            // ex:
+            // in the while loop....
+            //     wjherjwhejrhwejrh => "string is too large"
+            if(line.length() >= 10){
+                System.out.println("string is too large");
+            }
+
+            // part3. continue part2, if provided string is equals to your name
+            // program should print your surname
+            if(line.equals("Max")){
+                System.out.println("Adams");
+            }
         }
     }
 
