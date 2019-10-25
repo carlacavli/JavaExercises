b03138f06c3ea1db3c03011295a306a67586ddb7
TechnoStudy <info@techno.study>
2019-10-24 21:06:21 -0400

    day 14 code

---
+++ src/day14/FindMinimumInArray.java
@@ -0,0 +1,18 @@
+package day14;
+
+import javax.swing.*;
+
+public class FindMinimumInArray {
+
+//the task is to find and print out the minimum element
+
+    public static void main(String[] args) {
+        int[] myArray = {2, 11, 45, 9, 56};
+// 0   1   2   3  4
+
+        for (int i = 0; i < myArray.length; i++) {
+
+        }
+    }
+}
+
