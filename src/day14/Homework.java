a6f717f30a79dd4a11d785b32c0f38649f8dc1b3
TechnoStudy <info@techno.study>
2019-10-24 21:55:13 -0400

    day 14 code

---
+++ src/day14/Homework.java
@@ -0,0 +1,10 @@
+package day14;
+
+public class Homework {
+
+    //Task 1. write a for loop that finds an maximum for the array
+    //Task 2. write a for loop that finds an minimum for the array
+    //Task 3. write a for loop that finds an average for the array
+    // int[] myArray = {34,5,6,2,3,78,1}
+    //OUTPUT: average: (34+5+6+2+3+78+1)/7
+}
