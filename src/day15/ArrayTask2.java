1a34b869dff48a45c7640aae7223e013f79b0a3a
TechnoStudy <info@techno.study>
2019-10-26 14:17:50 -0400

    day 15 fixes

--- src/day15/ArrayTask2.java
+++ src/day15/ArrayTask2.java
@@ -1,7 +1,15 @@
 package day15;
 
+import java.util.Scanner;
+
 public class ArrayTask2 {
     //TODO
     //part1. using scanner fill array of length 10;
     //part2. print reverse of you array
+    // 1 2 3 4 5 6 7 8 9 10
+    // 10 9 8 7 6 5 4 3 2 1
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+    }
 }
--- src/day15/ArrayTask5.java
+++ src/day15/ArrayTask5.java
@@ -1,5 +1,6 @@
 package day15;
 
 public class ArrayTask5 {
-    //TODO Given 2 arrays of ints, a and b, return true if they have the same first ele
+    //TODO Given 2 arrays of ints, a and b, return true if they have the same
+    // first element
 }
