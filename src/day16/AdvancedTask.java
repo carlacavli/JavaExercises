7cb1b2dfa11dba28049afff81e57478879143fa9
TechnoStudy <info@techno.study>
2019-10-27 14:24:38 -0400

    day 16

--- src/day16/AdvancedTask.java
+++ src/day16/AdvancedTask.java
@@ -7,6 +7,11 @@ public class AdvancedTask {
     // *****
 
     //TODO  write program that prints this figures using loops
+    // *****
+    //  ***
+    //   *
+    
+    //TODO  write program that prints this figures using loops
     //   *
     //  ***
     // *****
--- src/day16/NestedLoopTask4.java
+++ src/day16/NestedLoopTask4.java
@@ -18,6 +18,11 @@ public class NestedLoopTask4 {
         System.out.println("USING LOOPS ----------------------");
         for (int i = 0; i <= 5; i++) {
             for (int j = 0; j <= 5; j++) {
+                if (j <= i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
             }
             System.out.println();
         }
