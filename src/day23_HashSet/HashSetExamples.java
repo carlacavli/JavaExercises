d635838668bb386db2bffe393cf17e3d852fa45e
AnwarAkhror <anwar.akhror@gmail.com>
2019-10-29 10:45:30 -0400

    course syllabus

--- src/day23_HashSet/HashSetExamples.java
+++ src/day23_HashSet/HashSetExamples.java
@@ -1,4 +1,5 @@
 package day23_HashSet;
 
 public class HashSetExamples {
+//    https://beginnersbook.com/2013/12/hashset-class-in-java-with-example/
 }
--- src/day24_Hashmap/HashMapImpl.java
+++ src/day24_Hashmap/HashMapImpl.java
@@ -1,4 +1,5 @@
 package day24_Hashmap;
 
 public class HashMapImpl {
+//    https://beginnersbook.com/2013/12/hashmap-in-java-with-example/
 }
