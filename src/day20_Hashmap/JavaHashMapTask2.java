28af2c5c582c39435bbd261964c28d826d31723e
TechnoStudy <info@techno.study>
2019-11-03 12:17:50 -0500

    java map task 3

--- src/day20_Hashmap/JavaHashMapTask2.java
+++ src/day20_Hashmap/JavaHashMapTask2.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 public class JavaHashMapTask2 {
     //2. Write a Java program to count the number
     //  of key-value (size) mappings in a map.
-
-
     public static void main(String[] args) {
         HashMap<Integer, String> map = new HashMap<>();
         map.put(1, "Red");
@@ -14,6 +12,7 @@ public class JavaHashMapTask2 {
         map.put(3, "Black");
         map.put(4, "White");
         map.put(5, "Blue");
-    }
 
+        System.out.println("Size of map is: " + map.size());
+    }
 }
