3ccb52724cf9ad1d91b510e02a6c23774e5ba0c4
TechnoStudy <info@techno.study>
2019-11-04 21:28:12 -0500

    practice

---
+++ src/day20_1_Practice/PutValue4.java
@@ -0,0 +1,25 @@
+package day20_1_Practice;
+
+import java.util.HashMap;
+
+public class PutValue4 {
+    // put letters as keys into your HashMap,
+    // put value = 0
+    public static void main(String[] args) {
+        String sentence = "hello";
+
+        HashMap<Character, Integer> letterMap = new HashMap<>();
+
+        char[] sentenceArray = sentence.toCharArray();
+//        for (int i = 0; i < sentenceArray.length; i++) {
+//
+//        }
+//        for (int i = 0; i <  sentence.length(); i++) {
+//            sentence.charAt(i);
+//        }
+        for (char letter : sentenceArray) {
+            letterMap.put(letter, 0);
+        }
+        System.out.println(letterMap);
+    }
+}
