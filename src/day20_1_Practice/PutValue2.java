4d91076691a5289b842bb7dedd5a8882a375eb7c
TechnoStudy <info@techno.study>
2019-11-04 20:16:47 -0500

    practice

--- src/day20_1_Practice/PutValue2.java
+++ src/day20_1_Practice/PutValue2.java
@@ -5,13 +5,16 @@ import java.util.HashMap;
 public class PutValue2 {
     // Put Strings into map as keys, and print you map
     public static void main(String[] args) {
-        String[] names = {"Max", "Drake", "Susan"};
+        String[] manyName = {"Max", "Drake", "Susan"};
         //write you code here
-
+        HashMap<String, Integer> technoStudy = new HashMap<>();
+        for (String singleName : manyName) {
+            technoStudy.put(singleName, 0);
+        }
         //end of you code
-        //System.out.println(map);
+        System.out.println(technoStudy);
         //output:
-        //{"Max"=0,"Drake"=0,"Susan"=0}
+        //{Max=0,Drake=0,Susan=0}
 
     }
 }
--- src/day20_1_Practice/PutValue3.java
+++ src/day20_1_Practice/PutValue3.java
@@ -2,7 +2,6 @@ package day20_1_Practice;
 
 public class PutValue3 {
     // Put first names and last names into your map, and print
-    //
     public static void main(String[] args) {
         String[] firstNames = {"Max", "Drake", "Susan"};
         String[] lastNames = {"Smith", "Jones", "Miller"};
@@ -11,6 +10,6 @@ public class PutValue3 {
         //end of you code
         //System.out.println(map);
         //output:
-        //{"Max"="Smith","Drake"="Jones","Susan"="Miller"}
+        //{Max=Smith,Drake=Jones,Susan=Miller}
     }
 }
