fcf5be7f0173aba105cae6174687b854322e3365
TechnoStudy <info@techno.study>
2019-11-04 20:10:39 -0500

    practice

---
+++ src/day20_1_Practice/PutValue2.java
@@ -0,0 +1,17 @@
+package day20_1_Practice;
+
+import java.util.HashMap;
+
+public class PutValue2 {
+    // Put Strings into map as keys, and print you map
+    public static void main(String[] args) {
+        String[] names = {"Max", "Drake", "Susan"};
+        //write you code here
+
+        //end of you code
+        //System.out.println(map);
+        //output:
+        //{"Max"=0,"Drake"=0,"Susan"=0}
+
+    }
+}
---
+++ src/day20_1_Practice/PutValue3.java
@@ -0,0 +1,16 @@
+package day20_1_Practice;
+
+public class PutValue3 {
+    // Put first names and last names into your map, and print
+    //
+    public static void main(String[] args) {
+        String[] firstNames = {"Max", "Drake", "Susan"};
+        String[] lastNames = {"Smith", "Jones", "Miller"};
+        //write you code here
+
+        //end of you code
+        //System.out.println(map);
+        //output:
+        //{"Max"="Smith","Drake"="Jones","Susan"="Miller"}
+    }
+}
