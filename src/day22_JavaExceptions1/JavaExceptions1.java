b80d4d66d68e991972d9af0ffd9caebb75753183
Techno Study <56236382+technoStudy@users.noreply.github.com>
2019-11-06 09:09:57 -0500

    Update JavaExceptions1.java

--- src/day22_JavaExceptions1/JavaExceptions1.java
+++ src/day22_JavaExceptions1/JavaExceptions1.java
@@ -1,5 +1,6 @@
 package day22_JavaExceptions1;
 
 public class JavaExceptions1 {
+//https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
 //    https://www.javatpoint.com/exception-handling-in-java
 }
