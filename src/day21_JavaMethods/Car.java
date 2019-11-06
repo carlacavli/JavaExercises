b3d20f813f1b981f7c093dbee4a5e1894380b6c4
TechnoStudy <info@techno.study>
2019-11-05 21:58:12 -0500

    homework day 21

---
+++ src/day21_JavaMethods/Car.java
@@ -0,0 +1,10 @@
+package day21_JavaMethods;
+
+public class Car {
+    public int speed;
+    public int doors;
+
+    public void Repair() {
+
+    }
+}
--- src/day21_JavaMethods/JavaMethods3.java
+++ src/day21_JavaMethods/JavaMethods3.java
@@ -1,6 +1,25 @@
 package day21_JavaMethods;
 
+import day22_JavaExceptions1.ComputerDesktop;
+
 import java.util.Scanner;
 
 public class JavaMethods3 {
+
+    public static void main(String[] args) {
+
+        Car myCar = new Car();
+
+        ComputerDesktop c = new ComputerDesktop();
+//        c.
+        
+        Vehicle v = new Vehicle();
+
+        v.model = "Fiat";
+        v.doors = 4;
+        v.speed = 65;
+        v.color = "White";
+        v.run();
+
+    }
 }
