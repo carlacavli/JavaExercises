2b95142e1890a59bfbf73d41078eec5bac98aa8c
TechnoStudy <info@techno.study>
2019-11-09 15:48:28 -0500

    day 22

---
+++ src/day22_Practice/Task8.java
@@ -0,0 +1,10 @@
+package day22_Practice;
+
+public class Task8 {
+    // Write a java program which will
+    // add only even numbers to list from array of integers
+    // {100,2,4,5,6,7,8,9,10}
+    // [2,4,6,8,10]
+
+    // hint: num%2==0 => this is even
+}
