8966a36f9abe94f00a2cd7ff583fa1316f2a112c
TechnoStudy <info@techno.study>
2019-11-10 11:12:50 -0500

    day 23

---
+++ src/day23_Practice/Task2.java
@@ -0,0 +1,9 @@
+package day23_Practice;
+
+public class Task2 {
+    // write a java program that find maximum
+    // of three numbers
+    public static void main(String[] args) {
+        
+    }
+}
