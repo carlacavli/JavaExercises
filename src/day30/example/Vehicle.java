d8fdaaa863493fd15e511be54cb46e306384e33d
TechnoStudy <info@techno.study>
2019-11-15 16:23:32 -0500

    fixes

---
+++ src/day30/example/Vehicle.java
@@ -0,0 +1,16 @@
+package day30.example;
+
+public class Vehicle {
+    int doors;
+    int speed;
+    String model;
+    String color;
+
+    void run() {
+        System.out.println("I am running");
+    }
+
+    private void run(int doors, int speed, String color) {
+        System.out.println("I am running");
+    }
+}
