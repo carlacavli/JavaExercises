7fdd342e106cc62ba69520874f8451c0cbf63b6a
TechnoStudy <info@techno.study>
2019-11-16 12:53:35 -0500

    day 32

--- src/day32/RecapClassObjectsMethods.java
+++ src/day32/RecapClassObjectsMethods.java
@@ -1,11 +1,16 @@
 package day32;
 
 public class RecapClassObjectsMethods {
+    //1
     // parameter
     // return
     // static
+
+    //2
     // class
     // class attributes
 
+    //3
     // other object as attribute
+    // call methods of other class
 }
