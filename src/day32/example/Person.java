afd298a5c413b576cc0106b7827919fb1b8eb472
TechnoStudy <info@techno.study>
2019-11-16 16:43:03 -0500

    day 32

---
+++ src/day32/example/Person.java
@@ -0,0 +1,16 @@
+package day32.example;
+
+public class Person {
+    
+    public String name;
+    public static String urlToGoogle = "google.com";
+
+    public static String getBody(){
+        return "Everybody has a body";
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
