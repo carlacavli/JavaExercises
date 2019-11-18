63a58e992bea30084eab6448302eb613da4f7f0f
TechnoStudy <info@techno.study>
2019-11-18 13:17:44 -0500

    day 32

---
+++ src/day33/task/Task1Github/src/Action.java
@@ -0,0 +1,4 @@
+package day33.task.Task1Github.src;
+
+public class Action {
+}
---
+++ src/day33/task/Task1Github/src/Pet.java
@@ -0,0 +1,14 @@
+package day33.task.Task1Github.src;
+
+public class Pet {
+    public String name;
+    public String breed;
+    public int legs;
+    public boolean cuteness;
+    public String gender;
+
+    public void canJump(){}
+    public void canFly(){}
+    public void canSwim(){}
+}
+
