6520f68b944594745d0d6fd56adfbfcdc056bd28
TechnoStudy <info@techno.study>
2019-11-19 21:53:26 -0500

    day 35 codes

--- src/day35/example/Constructor3.java
+++ src/day35/example/Constructor3.java
@@ -1,4 +1,19 @@
 package day35.example;
 
 public class Constructor3 {
+    private int data;
+
+    private Constructor3(){
+        data = 5;
+    }
 }
+class Test{
+    public static void main(String[] args) {
+        //Constructor3 c3 = new Constructor3();
+    }
+}
+//Answer a;
+//a) Compilation error
+//b) 5
+//c) Runtime error
+//d) None of these
