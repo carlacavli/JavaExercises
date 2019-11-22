3f8c91f0b7b0d1429e889f07b7b2fc4fcdab2d8b
TechnoStudy <info@techno.study>
2019-11-21 21:52:23 -0500

    day 36

--- src/day36/task/Task2.java
+++ src/day36/task/Task2.java
@@ -4,8 +4,15 @@ package day36.task;
 public class Task2 {
     public static void main(String[] args) {
         String[] arr = {"Hi", "Hello", null};
-
         // print first characters of each words
         // Hint: use charAt(0) to get first character of string
+
+        for (String word : arr) {
+            try {
+                System.out.println(word.substring(0, 1));
+            }catch (Exception ex){
+                System.out.println("We have non existing string");
+            }
+        }
     }
 }
---
+++ src/day36/task/Task4.java
@@ -0,0 +1,22 @@
+package day36.task;
+
+public class Task4 {
+
+    public static Integer covertToInt(String number) {
+        Integer integer = null;
+        try {
+            integer = Integer.valueOf(number);
+        } catch (Exception ex) {
+            System.out.println("cannot covert");
+        }
+
+        return integer;
+    }
+
+    public static void main(String[] args) {
+        // fix the code
+        System.out.println(covertToInt("one"));
+        System.out.println(covertToInt("2"));
+        System.out.println(covertToInt("-1"));
+    }
+}
